1483cd4eaf302508378dd58078ba89337d04d1d6
viet21092003 <21124548@st.hcmuaf.edu.vn>
2024-10-06 21:40:06 +0700

    fix push json file

--- file-service/src/main/java/com/studyapp/fileservice/FileServiceApplication.java
+++ file-service/src/main/java/com/studyapp/fileservice/FileServiceApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FileServiceApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(FileServiceApplication.class, args);
 	}
+
 }
